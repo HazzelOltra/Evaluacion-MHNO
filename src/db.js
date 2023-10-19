227d51638a87645b6aac8fcca575687cc7052e19
HazzelOltra <d20390034@huauchinango.tecnm.mx>
2023-10-18 18:22:28 -0600

    Actualizacion
    Agregamos el import con los datos para mysql

--- src/db.js
+++ src/db.js
@@ -1,10 +1,18 @@
 import { createPool } from "mysql2/promise";
 //revisar conexion a la base de datos creada
 
+import {
+DB_HOST,
+DB_PASSWORD,
+DB_PORT,
+DB_USER,
+DB_DATABASE
+} from './config.js'
+
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "Lolipop_60",
-  port: "3306",
-  database: 'pizzeria'
+  host:DB_HOST,
+  user:DB_USER,
+  password:DB_PASSWORD,
+  port:DB_PORT,
+  database:DB_DATABASE
 })
