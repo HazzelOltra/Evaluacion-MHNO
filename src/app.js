605b52f69bdc521a11325900bca83058ec591abc
HazzelOltra <d20390034@huauchinango.tecnm.mx>
2023-10-19 17:14:47 -0600

    Actualizacion env

--- src/app.js
+++ src/app.js
@@ -4,21 +4,19 @@ import pizzasRouter from "./Routes/pizzas.routes.js";
 import pedidosRouter from "./Routes/pedidos.routes.js";
 import empleadosRouter from "./Routes/empleados.routes.js";
 
-import './config.js'
+import "./config.js";
 const app = express();
 
 app.use(express.json());
-app.use('/api', pizzasRouter);
-app.use('/api', clientesRouter);
-app.use('/api', empleadosRouter);
-app.use('/api', pedidosRouter);
+app.use("/api", pizzasRouter);
+app.use("/api", clientesRouter);
+app.use("/api", empleadosRouter);
+app.use("/api", pedidosRouter);
 
-
-app.use((req,res,next)=>{
-    res.status(404).json({
-        Message: "Ruta no existente... verificar!"
-    })
-
-})
+app.use((req, res, next) => {
+  res.status(404).json({
+    Message: "Ruta no existente... verificar!",
+  });
+});
 
 export default app;
--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-import {config} from "dotenv";
-config()
+import { config } from "dotenv";
+config();
 
-export const PORT = process.env.PORT || 3600
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'Lolipop_60'
-export const DB_DATABASE = process.DB_DATABASE || 'pizzeria' 
-export const DB_HOST = process.DB_HOST || 'localhost'
-export const DB_PORT = process.DB_PORT || '3306'
+export const PORT = process.env.PORT || 3600;
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "Lolipop_60";
+export const DB_DATABASE = process.env.DB_DATABASE || "pizzeria";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_PORT = process.env.DB_PORT || "3306";
